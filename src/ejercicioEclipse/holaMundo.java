2ce9f7fc381f354fb752815cae78b195fb2ad1bd
Antonio Maria Crespo Casado <amcrespo@viewnext.com>
2021-04-13 10:10:26 +0200

    Enviar ejercicio

---
+++ src/ejercicioEclipse/holaMundo.java
@@ -0,0 +1,15 @@
+package ejercicioEclipse;
+
+public class holaMundo {
+
+	public static String saludo;
+	
+	public holaMundo (){
+		holaMundo.saludo="Hola buenos dias";
+	}
+
+
+	public String saludoDia(String saludo) {
+		return saludo;
+	}
+}
